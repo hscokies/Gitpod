648d45b7997c491666d3bff9e907c592d5ba7d7e
HScokies <hscokies@outlook.com>
2025-01-22 09:51:57 +0500

    feature: Add meta & compression

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'gzip',
+      threshold: 1025,
+      deleteOriginalAssets: false,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
