586916a6931a8fed46d026ba73115924486b8935
HScokies <hscokies@outlook.com>
2025-01-23 10:56:53 +0500

    fix: snackbar typo

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -7,7 +7,7 @@ export const App = () =>{
     <Header/>
     <RootPage/>
     <Snackbar buttonText='Accept Cookies'>
-      This website uses cookies to enhance the user experience. Read our <a>privacy policy</a> privacy policy
+      This website uses cookies to enhance the user experience. Read our <a href='/'>privacy policy</a> for more info.
     </Snackbar>
     <Footer/>
   </>
